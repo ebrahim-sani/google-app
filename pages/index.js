bb8261df7afb9bd7c87c5a80ed4abba45e94cb5d
ebrahim-sani <ibraheemsani035@gmail.com>
2021-04-11 12:00:50 +0100

    Built Body of the home screen

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import Image from "next/image";
 import Avatar from "../components/Avatar";
 import { ViewGridIcon, MicrophoneIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Home() {
   return (
@@ -26,6 +28,23 @@ export default function Home() {
       </header>
 
       {/* Body*/}
+      <form className="flex flex-col items-center mt-44 flex-grow">
+        <Image
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+          alt="google-logo"
+          height={100}
+          width={300}
+        />
+        <div className="flex sm:max-w-xl lg:max-w-2xl px-5 py-3 rounded-full border border-gray-200 w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow  focus:outline-none " />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">I Feels Excited</button>
+        </div>
+      </form>
 
       {/* Footer*/}
     </div>
